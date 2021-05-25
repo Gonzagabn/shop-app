f933cf8d49955f39a81dadbc1090b7ff96fd7e15
Gonzaga <luizgonzagabn@gmail.com>
2021-05-25 08:41:25 -0300

    commit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,10 @@
 class Constants {
-  static const BASE_API_URL = 'URL DB';
+  static const BASE_API_URL =
+      'https://"PROJECT_NAME"-default-rtdb.firebaseio.com';
   static const AUTH_SIGNUP_API_URL =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=API_KEY';
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="API_KEY"';
   static const AUTH_LOGIN_API_URL =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=API_KEY';
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="API_KEY"';
 }
 
 //Para funcionar local por exemplo:
